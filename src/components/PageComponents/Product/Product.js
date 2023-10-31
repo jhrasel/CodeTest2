4246ba0f334c35ed3665dd3a16af906f20b8dd6a
Md. Jafrul Hasan Rasel <jafrulras@gmail.com>
2023-11-01 01:36:16 +0600

    Pagination added done

--- src/components/PageComponents/Product/Product.js
+++ src/components/PageComponents/Product/Product.js
@@ -4,30 +4,37 @@ import { fetchProducts } from "./fetchProducts";
 import ORContainer from "@/components/Reuse/ORContainer";
 import { H2, H3, H4, H5, H6, P } from "@/components/Reuse/Tags";
 import ORImage from "@/components/Reuse/ORImage";
-
 import { TbCurrencyTaka } from "react-icons/tb";
-
-// Skeleton for loading
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import CustomPlaceholder from "./CustomPlaceholder";
 
+const ProductsPerPage = 8;
+
 const Product = () => {
   const [loading, setLoading] = useState(true);
-
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Current page
+  const [totalPages, setTotalPages] = useState(0); // Total number of pages
 
   useEffect(() => {
-    // Use the fetchProducts function to fetch the data
     fetchProducts()
       .then((data) => {
         setProducts(data);
+        setTotalPages(Math.ceil(data.length / ProductsPerPage));
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * ProductsPerPage;
+  const endIndex = currentPage * ProductsPerPage;
+
+  // Get the products for the current page
+  const currentPageProducts = products.slice(startIndex, endIndex);
 
   return (
     <>
@@ -36,7 +43,7 @@ const Product = () => {
           <H2 h2="Products" className="mb-8" />
 
           <div className="grid grid-cols-4 gap-7">
-            {products.map((product, index) => (
+            {currentPageProducts.map((product, index) => (
               <div className="p-5 border-2 border-slate-200" key={index}>
                 {/* images */}
                 <div className="text-center">
@@ -72,6 +79,23 @@ const Product = () => {
                   </button>
                 </div>
               </div>
+            ))}
+          </div>
+
+          {/* Pagination controls */}
+          <div className="mt-5 flex justify-center">
+            {Array.from({ length: totalPages }, (_, i) => (
+              <button
+                key={i}
+                className={`mx-2 px-4 py-2 rounded-full ${
+                  currentPage === i + 1
+                    ? "bg-orange-400 text-white"
+                    : "bg-gray-200"
+                }`}
+                onClick={() => setCurrentPage(i + 1)}
+              >
+                {i + 1}
+              </button>
             ))}
           </div>
         </ORContainer>
