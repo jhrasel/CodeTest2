7cefcb5247cd7941d3290538c00ff18e3f803344
Md. Jafrul Hasan Rasel <jafrulras@gmail.com>
2023-11-01 23:20:17 +0600

    update product page also add in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Product from "@/components/PageComponents/Product/Product";
 import { H2 } from "@/components/Reuse/Tags";
 
 export default function Home() {
   return (
     <>
-      <H2 h2="Home" />
+      <Product />
     </>
   );
 }
