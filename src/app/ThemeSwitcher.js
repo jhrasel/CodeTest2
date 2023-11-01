dffc0f23266c1be191168f2a18bf9e3e77c1329c
Md. Jafrul Hasan Rasel <jafrulras@gmail.com>
2023-11-01 23:05:16 +0600

    change

--- src/app/ThemeSwitcher.js
+++ src/app/ThemeSwitcher.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from "react";
+"use client";
 import ORImage from "@/components/Reuse/ORImage";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  const [open, setOpen] = useEffect(() => {
-    setOpen(JSON.parse(localStorage.getItem("themeSwitcherOpen")) || false);
-  }, []);
+  //   const [open, setOpen] = useState(
+  //     // Initialize open state from localStorage or default to false
+  //     JSON.parse(localStorage.getItem("themeSwitcherOpen")) || false
+  //   );
 
-  const handleOpen = () => {
-    const newOpen = !open;
-    setOpen(newOpen);
-    localStorage.setItem("themeSwitcherOpen", JSON.stringify(newOpen));
-  };
+  //   const handleOpen = () => {
+  //     const newOpen = !open;
+  //     setOpen(newOpen);
+  //     // Save the open state to localStorage
+  //     localStorage.setItem("themeSwitcherOpen", JSON.stringify(newOpen));
+  //   };
 
   useEffect(() => {
     setMounted(true);
@@ -24,23 +27,52 @@ const ThemeSwitcher = () => {
   }
 
   return (
+    // <div className="fixed top-0 right-0">
+    //   {open ? (
+    //     <div className="" onClick={handleOpen}>
+    //       <button
+    //         onClick={() => setTheme("light")}
+    //         className="border-2 bg-orange-400 p-1 rounded-lg"
+    //       >
+    //         <ORImage
+    //           image="/images/light-mood.png"
+    //           width="30"
+    //           height="30"
+    //           alt="light"
+    //         />
+    //       </button>
+    //     </div>
+    //   ) : (
+    //     <div className="" onClick={handleOpen}>
+    //       <button
+    //         onClick={() => setTheme("dark")}
+    //         className="border-2 bg-orange-400 p-1 rounded-lg"
+    //       >
+    //         <ORImage
+    //           image="/images/dark-mood.png"
+    //           width="30"
+    //           height="30"
+    //           alt="light"
+    //         />
+    //       </button>
+    //     </div>
+    //   )}
+    // </div>
+
     <div className="fixed top-0 right-0">
-      {open ? (
-        <div className="" onClick={handleOpen}>
-          <button
-            onClick={() => setTheme("light")}
-            className="border-2 bg-orange-400 p-1 rounded-lg"
-          >
-            <ORImage
-              image="/images/light-mood.png"
-              width="30"
-              height="30"
-              alt="light"
-            />
-          </button>
-        </div>
-      ) : (
-        <div className="" onClick={handleOpen}>
+      <div className="">
+        <button
+          onClick={() => setTheme("light")}
+          className="border-2 bg-orange-400 p-1 rounded-lg"
+        >
+          <ORImage
+            image="/images/light-mood.png"
+            width="30"
+            height="30"
+            alt="light"
+          />
+        </button>
+        <div className="">
           <button
             onClick={() => setTheme("dark")}
             className="border-2 bg-orange-400 p-1 rounded-lg"
@@ -53,7 +85,7 @@ const ThemeSwitcher = () => {
             />
           </button>
         </div>
-      )}
+      </div>
     </div>
   );
 };
